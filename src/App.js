2b427ab53ee1a7dad122731c0c7e25a00b39e2e0
Madi <Madison-CF@hotmail.fr>
2023-02-15 12:04:14 +0100

    routes ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Routes, Route, NavLink } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Accueil from "./pages/Accueil"
+import Apropos from "./pages/Apropos"
+import Services from "./pages/Services"
+import Contact from "./pages/Contact"
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Accueil/>}></Route>
+      <Route path="/apropos" element={<Apropos/>}></Route>
+      <Route path="/services" element={<Services/>}></Route>
+      <Route path="/contact" element={<Contact/>}></Route>
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+const Navbar = () => {
+    return(
+        <section class="navbar">
+            <ul>
+                <li>
+                    Accueil 
+                </li>
+                <li>
+                    Qui Sommes-nous?
+                </li>
+                <li>
+                    Services
+                </li>
+                <li>
+                    Contact
+                </li>
+            </ul>
+        </section>
+    )
+} 
+
+export default Navbar;

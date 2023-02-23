f10ef0543cc65d428dfa04f782a6e08bfa4e84b1
Madi <Madison-CF@hotmail.fr>
2023-02-23 15:28:37 +0100

    recruitment section ok

--- src/components/Recruitment.js
+++ src/components/Recruitment.js
@@ -5,24 +5,34 @@ const Recruitment = () => {
     <section className="recruitment">
       <h2 className="recruitment__title sectionTitle">Nous recrutons !</h2>
       <div className="recruitment__container">
-        <img className="recruitment__container__img"src={enroll} alt="personne accompagnant une personne âgée"></img>
+        <img
+          className="recruitment__container__img"
+          src={enroll}
+          alt="personne accompagnant une personne âgée"
+        ></img>
         <div className="recruitment__container__announcement">
           <p>
             AMC recrute des assistant·e·s de vie à domicile titulaires d'un
             diplôme professionnel niveau 5, des aides à domicile, des aides
-            ménagères ainsi que des accompagnant·e·s. Si vous êtes en
-            reconversion professionnelle, nous pouvons également vous faire
-            bénéficier d'une formation en interne. Si vous partagez les mêmes
-            valeurs que nous, n'attendez plus! Envoyez-nous votre CV ainsi
-            qu'une lettre de motivation à l'adresse suivante:
+            ménagères ainsi que des accompagnant·e·s.
+            <br />
+            <br /> Si vous êtes en reconversion professionnelle, nous pouvons
+            également vous faire bénéficier d'une formation en interne.
+            <br />
+            <br /> Si vous partagez les mêmes valeurs que nous, n'attendez plus!
+            Envoyez-nous votre CV ainsi qu'une lettre de motivation à l'adresse
+            suivante:
           </p>
           <div className="recruitment__container__announcement__address">
-            701 rue Guy Pernin 54200 Toul{" "}
+            701 rue Guy Pernin <br /> 54200 Toul{" "}
           </div>
           <p>ou envoyez-nous CV et lettre de motivation par email:</p>
-          <button className="recruitment__container__announcement__btn">
-            En cliquant ici!{" "}
-          </button>
+          <a
+            className="recruitment__container__announcement__btn"
+            href="mailto: toul@amcsap.fr"
+          >
+            En cliquant ici !
+          </a>
         </div>
       </div>
       <div className="recruitment__background"></div>

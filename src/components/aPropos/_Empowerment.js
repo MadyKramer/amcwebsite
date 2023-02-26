c16293bd065c5c0ec21f4e5cf9d3d0e203a835fc
Madi <Madison-CF@hotmail.fr>
2023-02-26 13:23:52 +0100

    empowerment style

--- src/components/aPropos/_Empowerment.js
+++ src/components/aPropos/_Empowerment.js
@@ -1,5 +1,33 @@
 const Empowerment = () => {
-    return
-}
+  return (
+    <section className="empowerment">
+        <h2 className="sectionTitle">Agréments et habilitations</h2>
+      <div className="empowerment__content">
+        <div className="empowerment__content__left">
+          <ul>
+            <li>Conseil départemental de Meurthe et Moselle</li>
+            <li>CARSAT</li>
+            <li>AGIRC-ARRCO "Sortir Plus"</li>
+            <li>RMA "Ressources Mutuelles Assitance</li>
+            <li>Sécurité Sociale Militaire</li>
+            <li>CESU</li>
+          </ul>
+        </div>
+        <div className="empowerment__content__right">
+          <h3>AMC adhère à:</h3>
+          <ul>
+            <li> la charte nationale qualité des services à la personne</li>
+            <li>
+              {" "}
+              la Fédésap (Fédération Française des Services à la Personne et de
+              Proximité)
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="empowerment__background"></div>
+    </section>
+  );
+};
 
 export default Empowerment;

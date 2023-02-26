1738f11a0c644378b91134ee9cf7bd2623ecc31d
Madi <Madison-CF@hotmail.fr>
2023-02-26 13:26:00 +0100

    aPropos Ok

--- src/pages/Apropos.js
+++ src/pages/Apropos.js
@@ -1,6 +1,7 @@
 import About from "../components/aPropos/About";
 import Historical from "../components/aPropos/Historical";
 import Empowerment from "../components/aPropos/_Empowerment";
+import Footer from "../components/Footer";
 
 const Apropos = () => {
   return (
@@ -8,6 +9,7 @@ const Apropos = () => {
       <About />
       <Historical />
       <Empowerment />
+      <Footer />
     </main>
   );
 };

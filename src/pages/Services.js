735bbc1cc93fe6b4f6e8faf957606fe9f62dc8c1
Madi <Madison-CF@hotmail.fr>
2023-02-27 12:31:36 +0100

    support

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,12 +1,13 @@
 import ServicesHome from "../components/ServicesHome"
 import Description from "../components/services/Description";
+import Support from "../components/services/Support";
 
 const Services = () => {
   return (
     <main>
       <Description />
       <ServicesHome />
-
+      <Support />
     </main>
   );
 };
